7da70924cce12d7365e34ec60c517b29bbfe3871
Marat <marat@deex.exchange>
2019-01-24 20:50:08 +0300

    fix mobile

--- src/main.js
+++ src/main.js
@@ -2,4 +2,33 @@ require('./sass/main.scss')
 require('./index.html')
 require('./js/seed.js');
 
-console.log(Seed.api);
+
+class List {
+  constructor(el) {
+    this.el = el
+    this.data = Seed.api
+  }
+
+  static createListItem(text) {
+    const li = document.createElement('li');
+    li.textContent = text
+    return li
+  }
+
+  update() {
+    while (this.el.firstChild) {
+      this.el.removeChild(this.el.firstChild);
+    }
+
+    for (const text of this.data) {
+      console.log(text.from);
+      this.el.appendChild(List.createListItem(text.from));
+    }
+  }
+
+}
+
+// const listBox = document.getElementById('listBox');
+// const listBinding = new List(listBox);
+
+// listBinding.update();
